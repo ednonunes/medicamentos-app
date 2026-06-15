54ef5ab6bd8992b480f964d91f29f12799ebaf70
Edno Nunes <edno.nunes@farmaciaindiana.com.br>
2026-06-15 11:32:07 -0300

    atualizada migration demedicamentos

--- database/migrations/2026_06_15_140136_create_medications_table.php
+++ database/migrations/2026_06_15_140136_create_medications_table.php
@@ -10,12 +10,19 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('medications', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('medications', function (Blueprint $table) {
+        $table->id();
+        // Vincula o medicamento ao usuário que está logado
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        
+        $table->string('name');            // Nome do remédio
+        $table->string('dosage');          // ex: 500mg, 10 gotas
+        $table->integer('interval_hours'); // ex: 8, 12
+        $table->time('start_time');        // Horário da primeira dose
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
