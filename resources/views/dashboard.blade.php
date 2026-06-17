6a865b1166deb9ee96e38906b40ed5b5146eb160
Edno Nunes <edno.nunes@farmaciaindiana.com.br>
2026-06-17 13:11:03 -0300

    alterado dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,10 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    Este é seu painel para cadastro de medicamentos e horários, bem como a visualização de sua agenda, basta nagevar pelo menu e começar.<br>
+                    Em breve mais informações aqui. <br>
                 </div>
             </div>
         </div>
